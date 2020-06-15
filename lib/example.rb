9f0a8d131c87f88ab8c590c67f3e6c10343e72ac
Lisa Feng <lisafeng10@gmail.com>
2020-06-15 13:23:47 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,4 @@
 def say_hello_world_ten_times
-
   phrase = "Hello World!"
   puts phrase
   puts phrase
@@ -7,9 +6,12 @@ def say_hello_world_ten_times
   puts phrase
   puts phrase
   puts phrase
-    puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
 
+def greeting
+  puts "Hello World"
 end
